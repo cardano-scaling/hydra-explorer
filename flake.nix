ed11415b0c2430168358b228224a066397dba64f
Daniel Firth <daniel.firth@iohk.io>
2024-10-16 11:57:38 +0000

    Add system image

--- flake.nix
+++ flake.nix
@@ -1,9 +1,25 @@
-# Docs for this file: https://github.com/input-output-hk/iogx/blob/main/doc/api.md#flakenix
 {
-  description = "Change the description field in your flake.nix";
-
+  description = "hydra-explorer application and system image";
 
   inputs = {
+
+    CHaP = {
+      url = "github:IntersectMBO/cardano-haskell-packages?ref=repo";
+      flake = false;
+    };
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    hackage = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
+
+    haskell-nix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.hackage.follows = "hackage";
+    };
+
     iogx = {
       url = "github:input-output-hk/iogx";
       inputs.hackage.follows = "hackage";
@@ -12,38 +28,73 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nixinate.url = "github:MatthewCroughan/nixinate";
+
     nixpkgs.follows = "haskell-nix/nixpkgs";
 
-    hackage = {
-      url = "github:input-output-hk/hackage.nix";
-      flake = false;
-    };
-
-    CHaP = {
-      url = "github:IntersectMBO/cardano-haskell-packages?ref=repo";
-      flake = false;
-    };
-
-    haskell-nix = {
-      url = "github:input-output-hk/haskell.nix";
-      inputs.hackage.follows = "hackage";
-    };
   };
 
+  outputs = inputs:
+    let
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ];
+    in
+    inputs.iogx.lib.mkFlake
+      {
 
-  # Docs for mkFlake: https://github.com/input-output-hk/iogx/blob/main/doc/api.md#mkflake
-  outputs = inputs: inputs.iogx.lib.mkFlake {
+        inherit inputs;
+        inherit systems;
 
-    inherit inputs;
+        repoRoot = ./.;
 
-    repoRoot = ./.;
+        outputs = import ./nix/outputs.nix;
 
-    outputs = import ./nix/outputs.nix;
+      } //
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      inherit systems;
+      flake = {
 
-    systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
+        apps = inputs.nixinate.nixinate."x86_64-linux" inputs.self;
 
-  };
+        nixosConfigurations = {
+          hydra-explorer = inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = inputs;
+            modules = [
+              {
+                _module.args.nixinate = {
+                  host = "explorer.hydra.family";
+                  sshUser = "root";
+                  buildOn = "local";
+                  substituteOnTarget = true;
+                  hermetic = false;
+                };
+              }
+              {
+                imports = [ "${inputs.nixpkgs}/nixos/modules/virtualisation/amazon-image.nix" ];
 
+                networking.hostName = "hydra-explorer";
+
+                nix.settings.trusted-users = [ "root" ];
+
+                services.openssh.enable = true;
+
+                system.stateVersion = "24.05";
+
+                networking.firewall = {
+                  allowedTCPPorts = [ 25 80 443 ];
+                  enable = false;
+                };
+              }
+            ];
+          };
+        };
+      };
+    };
 
   nixConfig = {
     extra-substituters = [
@@ -54,4 +105,5 @@
     ];
     allow-import-from-derivation = true;
   };
+
 }
