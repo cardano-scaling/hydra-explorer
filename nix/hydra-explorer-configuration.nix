927a7c4939ce0e210546c0c0d790089aca360c90
Noon van der Silk <noon.vandersilk@iohk.io>
2024-11-07 16:51:02 +0000

    More access to deploy

--- nix/hydra-explorer-configuration.nix
+++ nix/hydra-explorer-configuration.nix
@@ -23,6 +23,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIATz/Jv+AnBft+9Q01UF07OydvgTTaTdCa+nMqabkUNl" # noonio
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEMuBv9vXsKsOsjS7B6zMOpuLw+gGGHR6hADuTeiNfKO" # locallycompact
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBoR/OGSvjN2PCi2+OGeFTSTWR0aacCwK41j1gUu5UGr" # v0d1ch
     ];
   };
 
