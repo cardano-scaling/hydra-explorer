b468b45a18227986489253849366485d1434e361
Franco Testagrossa <franco.testagrossa@iohk.io>
2025-02-28 12:45:54 +0100

    add light gray border to tickbox

--- hydra-explorer/web/src/components/TickBox/index.tsx
+++ hydra-explorer/web/src/components/TickBox/index.tsx
@@ -38,8 +38,8 @@ const TickBox = () => {
             {error ? (
                 <p className="text-red-500">{error}</p>
             ) : (
-                <div className="w-full">
-                    <table className="table-fixed bg-gray-800 text-white rounded-lg">
+                <div className="w-full border border-gray-700 rounded-lg">
+                    <table className="table-fixed bg-gray-800 text-white rounded-lg border-collapse">
                         <thead>
                             <tr>
                                 <th className="text-center px-4 py-2">Block Number</th>
