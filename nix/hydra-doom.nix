c99ae65bef1a46c06059bb791b4a8f48f9a66797
Sebastian Nagel <sebastian.nagel@ncoding.at>
2025-06-18 12:59:00 +0200

    Reverse proxy + TLS secure the doom-api.hydra.family

--- nix/hydra-doom.nix
+++ nix/hydra-doom.nix
@@ -3,8 +3,24 @@
 { pkgs, lib, inputs, config, system, ... }:
 
 {
-  # TODO: reverse proxy doom.hydra.family
-  networking.firewall.allowedTCPPorts = [ 8000 4001 ];
+  services.nginx.virtualHosts."doom-api.hydra.family" = {
+    forceSSL = true;
+    enableACME = true;
+    locations."/" = {
+      # NOTE: hydra-control-plane running on 8000 via process compose
+      proxyPass = "http://127.0.0.1:8000";
+      proxyWebsockets = true;
+      extraConfig = ''
+        proxy_buffering off;
+        client_max_body_size 500M;
+      '';
+    };
+  };
+
+  # Open port for hydra-node
+  networking.firewall.allowedTCPPorts = [ 4001 ];
+
+  # NOTE: Process compose including the hydra-control-plane and a hydra-node
   environment.systemPackages = with pkgs; [
     # alias to hydra-doom-process-compose
     (pkgs.writeShellScriptBin "hydra-doom-process-compose" ''
@@ -12,4 +28,5 @@
     '')
   ];
   # TODO: start hydra-doom's process-compose in systemd service
+  # TODO: switch to docker or dedicated systemd services?
 }
