e1c38923e0b9f97af6a348fe7dff28f8d29782a3
Noon van der Silk <noon.vandersilk@iohk.io>
2025-10-03 13:18:12 +0100

    attempt to bring back packages; but failing

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -11,7 +11,59 @@ let
     inherit inputs pkgs lib project ghc;
   };
 
-  packages = {};
+  packages = rec {
+    hydra-explorer-web = import ../hydra-explorer/web/hydra-explorer.nix
+    {
+      inherit pkgs;
+    };
+
+    # Broken; we've lost this 'cross' attribute; there's a different way
+    # to do it now:
+    #
+    # <https://input-output-hk.github.io/haskell.nix/tutorials/cross-compilation.html?highlight=cross#cross-compilation>
+    #
+    hydra-explorer-static = builtins.throw "Broken";
+      # project.cross.musl64.cabalProject.hsPkgs.hydra-explorer.components.exes.hydra-explorer;
+
+
+    # Broken due to above being broken.
+    docker = pkgs.dockerTools.streamLayeredImage {
+      name = "hydra-explorer";
+      tag = "latest";
+      created = "now";
+      config = {
+        Entrypoint = [ "${hydra-explorer-static}/bin/hydra-explorer" ];
+        WorkingDir = "/";
+      };
+      # Copy the static files to /static in the docker image
+      contents = [
+        (pkgs.runCommand "hydra-explorer-static-files" { } ''
+          mkdir $out
+          ln -s ${hydra-explorer-web} $out/static
+        '')
+      ];
+    };
+
+    # Broken; I don't know why.
+    # A place to hack on the image to see how it works.
+    qemu = inputs.nixos-generators.nixosGenerate {
+      inherit system;
+      specialArgs = {
+        inherit inputs;
+        diskSize = 10 * 1024;
+      };
+
+      format = "qcow";
+
+      modules = [
+        (import ./hydra-explorer-configuration.nix {
+          cardano-node-module = inputs.cardano-node.nixosModules.cardano-node;
+          hydra-explorer = inputs.self.packages.x86_64-linux.hydra-explorer;
+          hydra-explorer-web = inputs.self.packages.x86_64-linux.hydra-explorer-web;
+        })
+      ];
+    };
+  };
 
   devShells = rec {
     default = mkShell "ghc966";
@@ -19,77 +71,6 @@ let
 
 in
   {
-    # xx = builtins.throw "xx";
-    explorer = builtins.throw "bleh";
-
-    # nixosConfigurations.explorer =
-    #   inputs.nixpkgs.lib.nixosSystem
-    #     {
-    #       system = "x86_64-linux";
-    #       specialArgs = inputs;
-    #       modules = [
-    #         {
-    #           imports = [
-    #             "${inputs.nixpkgs}/nixos/modules/virtualisation/amazon-image.nix"
-    #             (import ./nix/hydra-explorer-configuration.nix)
-    #           ];
-    #         }
-    #         inputs.agenix.nixosModules.default
-    #       ];
-    #     };
-
     inherit packages;
     inherit devShells;
   }
-
-# { repoRoot, inputs, pkgs, lib, system }:
-
-# let
-#   project = repoRoot.nix.project;
-# in
-# [
-#   project.flake
-#   rec {
-#     packages.hydra-explorer-web = import ../hydra-explorer/web/hydra-explorer.nix { inherit pkgs; };
-
-#     packages.hydra-explorer-static =
-#       project.cross.musl64.cabalProject.hsPkgs.hydra-explorer.components.exes.hydra-explorer;
-
-#     packages.docker = pkgs.dockerTools.streamLayeredImage {
-#       name = "hydra-explorer";
-#       tag = "latest";
-#       created = "now";
-#       config = {
-#         Entrypoint = [ "${packages.hydra-explorer-static}/bin/hydra-explorer" ];
-#         WorkingDir = "/";
-#       };
-#       # Copy the static files to /static in the docker image
-#       contents = [
-#         (pkgs.runCommand "hydra-explorer-static-files" { } ''
-#           mkdir $out
-#           ln -s ${packages.hydra-explorer-web} $out/static
-#         '')
-#       ];
-#     };
-
-#     # A place to hack on the image to see how it works.
-#     packages.qemu = inputs.nixos-generators.nixosGenerate {
-#       inherit system;
-#       specialArgs = {
-#         inherit
-#           inputs;
-#         diskSize = 10 * 1024;
-#       };
-
-#       format = "qcow";
-
-#       modules = [
-#         (import ./hydra-explorer-configuration.nix {
-#           cardano-node-module = inputs.cardano-node.nixosModules.cardano-node;
-#           hydra-explorer = inputs.self.packages.x86_64-linux.hydra-explorer;
-#           hydra-explorer-web = inputs.self.packages.x86_64-linux.hydra-explorer-web;
-#         })
-#       ];
-#     };
-#   }
-# ]
--- nix/shell.nix
+++ nix/shell.nix
@@ -3,9 +3,6 @@ let
   allTools = {
     "ghc966".cabal                    = project.projectVariants.ghc966.tool "cabal" "latest";
     "ghc966".haskell-language-server  = project.projectVariants.ghc966.tool "haskell-language-server" "latest";
-    # "ghc966".cabal-fmt                = project.projectVariants.ghc966.tool "cabal-fmt" "latest";
-    # "ghc966".stylish-haskell          = project.projectVariants.ghc966.tool "stylish-haskell" "latest";
-    # "ghc966".hlint                    = project.projectVariants.ghc966.tool "hlint" "latest";
   };
 
   tools = allTools.${ghc};
@@ -19,9 +16,9 @@ let
     pkgs.hydra-chain-observer
     pkgs.hydra-node
 
-    pkgs.yarn
-    pkgs.check-jsonschema
-    pkgs.isd
+    pkgs.check-jsonschema # For hydra-explorer:test:tests
+    pkgs.yarn # For hacking on the UI
+    pkgs.isd # interactive systemd viewer
   ];
 
   shell = project.shellFor {
@@ -32,27 +29,5 @@ let
 
     withHoogle = true;
   };
-
 in
-
   shell
-
-
-# Docs for this file: https://github.com/input-output-hk/iogx/blob/main/doc/api.md#mkhaskellprojectinshellargs
-# See `shellArgs` in `mkHaskellProject` in ./project.nix for more details.
-
-# { repoRoot, inputs, pkgs, lib, system }:
-
-# cabalProject:
-
-# {
-#   name = "nix-shell";
-#   packages = [
-#     pkgs.cardano-cli
-#     pkgs.cardano-node
-#     pkgs.hydra-chain-observer
-#     pkgs.hydra-node
-#     pkgs.check-jsonschema # For hydra-explorer:test:tests
-#     pkgs.yarn # For hacking on the UI
-#   ];
-# }
