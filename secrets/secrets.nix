5002a8690456953096b003d2aa5248adab1c11cb
Noon van der Silk <noon.vandersilk@iohk.io>
2026-07-14 13:42:28 +0100

    keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,11 @@
 let
   users = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEMuBv9vXsKsOsjS7B6zMOpuLw+gGGHR6hADuTeiNfKO" # locallycompact
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBoR/OGSvjN2PCi2+OGeFTSTWR0aacCwK41j1gUu5UGr" # v0d1ch
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBtamRlrHLKLzr8Pcm3qEgdbJh7vCjMO4tm0wbW3REYL" # ffakenz
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDDeluqrRI+soqfLSupug/dp8AxZ5S+gplFC+8YPzbM" # ch1bo (hydra-explorer)
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBJd9BiDoUNl0pCVDeIKnlwJu6oOmLIz7l3Ct7xoYjBS" # noonio
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF/nkV4og13MWwILyhxQ2n3NWb2QQ4HqTuKgE9YmbIOx" # vrom911
   ];
-  hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICSZnoCbesoGNLpB3U5JPJrXE/WYpKI2ae4JPLrLKlHV";
+  hostKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAu90XurvqxizckbEAViD/X1VMLS7U5FsZ0nbx1KRAes";
 in
 {
   "github-runner-token.age".publicKeys = users ++ [ hostKey ];
